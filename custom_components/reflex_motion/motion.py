e5c0d86b62e50ffb7e178170a7610fa6194af4ab
Khaleel Al-Adhami <khaleel.aladhami@gmail.com>
2024-11-04 14:52:40 -0800

    add more fields to motion

--- custom_components/reflex_motion/motion.py
+++ custom_components/reflex_motion/motion.py
@@ -3,8 +3,13 @@
 # For wrapping react guide, visit https://reflex.dev/docs/wrapping-react/overview/
 
 import reflex as rx
-from typing import Any, Dict, List, Optional, Set, Union
+from typing import Dict, List, Union
 
+MOTION_PRIMITIVE_VALUE = Union[float, str, int, List[Union[float, str, int]]]
+
+MOTION_MAPPING = Dict[str, MOTION_PRIMITIVE_VALUE]
+
+MOTION_VALUE = Dict[str, Union[MOTION_PRIMITIVE_VALUE, MOTION_MAPPING]]
 
 class Motion(rx.Component):
     """Motion component."""
@@ -16,28 +21,47 @@ class Motion(rx.Component):
     tag = "motion.div"
 
     # The initial state of the component.
-    initial: rx.Var[Dict[str, Union[float, str]]]
+    initial: rx.Var[MOTION_VALUE]
 
     # The animate state of the component.
-    animate: rx.Var[Dict[str, Union[float, str]]]
+    exit: rx.Var[MOTION_VALUE]
+
+    # The animate state of the component.
+    animate: rx.Var[MOTION_VALUE]
 
     # The transition 
-    transition: rx.Var[Dict[str, Union[float, str]]]
+    transition: rx.Var[MOTION_VALUE]
 
     # What the component does when it's hovered.
-    while_hover: rx.Var[Dict[str, Union[float, str]]]
+    while_hover: rx.Var[MOTION_VALUE]
 
     # What the component does when it's tapped.
-    while_tap: rx.Var[Dict[str, Union[float, str]]]
+    while_tap: rx.Var[MOTION_VALUE]
 
     # What the component does when it's in view.
-    while_in_view: rx.Var[Dict[str, Union[float, str]]]
+    while_in_view: rx.Var[MOTION_VALUE]
 
     # What the component does when its focused.
-    while_focus: rx.Var[Dict[str, Union[float, str]]]
+    while_focus: rx.Var[MOTION_VALUE]
 
     # What the component does when it's out of view.
-    viewport: rx.Var[Union[str, List[str]]]
+    viewport: rx.Var[Dict[str, Union[str, bool, int, float]]]
 
+    # If the component should animate on layout changes.
+    layout: rx.Var[bool]
+
+    # If the component is the root of the layout.
+    layout_root: rx.Var[bool]
 
 motion = Motion.create
+
+class AnimatePrescence(rx.Component):
+    """AnimatePresence component."""
+
+    # The React library to wrap.
+    library = "framer-motion"
+
+    # The React component tag.
+    tag = "AnimatePresence"
+
+animate_presence = AnimatePrescence.create
